fa9972668328bbb9967b12800e695eeef2cd128f
Your Name <you@example.com>
2022-07-19 20:57:46 +0300

    added some navigation remapping and incomplete deleting

--- main.ts
+++ main.ts
@@ -15,67 +15,214 @@ export default class MyPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+			id: 'navigate-to-next-character',
+			name: 'Navigate to next character',
+			editorCallback: (editor: Editor) => {
+
+				function code(): void {
+					navNextChar(editor)
+				}
+
+				repeatUntilKeyUp(code, this)
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
+
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+			id: 'navigate-to-previous-character',
+			name: 'Navigate to previous character',
+			editorCallback: (editor: Editor) => {
+
+				function code(): void {
+					navPrevChar(editor)
+				}
+
+				repeatUntilKeyUp(code, this)
+			}
+		});
+
+		this.addCommand({
+			id: 'navigate-to-next-word',
+			name: 'Navigate to next word',
+			editorCallback: (editor: Editor) => {
+
+				/* METHOD
+				* 2 modes, special char mode (no 0) and normal char mode (no 1).
+				* skip white spaces only as first chars otherwise stop
+				* */
+				function code(): void {
+
+					const SPECIAL_CHAR_WORD = 0
+					const NORMAL_CHAR_WORD = 1
+
+					let currentLineStr = editor.getLine(editor.getCursor().line)
+
+					// check if at the end of line
+					if ( currentLineStr.length <= editor.getCursor().ch ) {
+
+						navNextChar(editor) // moves to next line
+
+						return
 					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+					let followingChar = currentLineStr.charAt( editor.getCursor().ch );
+					let currentMode = NORMAL_CHAR_WORD;
+
+					// first skip initial whitespaces
+					while ( followingChar == " ") {
+
+						navNextChar(editor);
+
+						followingChar = currentLineStr.charAt( editor.getCursor().ch );
+					}
+
+					if ( !isNormalChar(followingChar) ) currentMode = SPECIAL_CHAR_WORD;
+
+					// skip characters until hit a special one or vice versa
+					while ( currentMode == isNormalChar( followingChar ) && followingChar != " ") {
+
+						navNextChar(editor);
+
+						followingChar = currentLineStr.charAt( editor.getCursor().ch )
+					}
 				}
+				repeatUntilKeyUp(code, this)
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addCommand({
+			id: 'navigate-to-previous-word',
+			name: 'Navigate to previous word',
+			editorCallback: (editor: Editor) => {
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+				/* METHOD
+				* 2 modes, special char mode (no 0) and normal char mode (no 1).
+				* skip white spaces only as first chars otherwise stop
+				* */
+				function code(): void {
+
+					const SPECIAL_CHAR_WORD = 0
+					const NORMAL_CHAR_WORD = 1
+
+					let currentLineStr = editor.getLine(editor.getCursor().line)
+
+					// check if at the end of line
+					if (  editor.getCursor().ch == 0 ) {
+
+						navPrevChar(editor) // moves to next line
+						return
+					}
+
+					let followingChar = currentLineStr.charAt( editor.getCursor().ch-1 );
+					let currentMode = NORMAL_CHAR_WORD;
+
+					// first skip initial whitespaces
+					while ( followingChar == " ") {
+
+						navPrevChar(editor);
+
+						followingChar = currentLineStr.charAt( editor.getCursor().ch-1 );
+					}
+
+					if ( !isNormalChar(followingChar) ) currentMode = SPECIAL_CHAR_WORD;
+
+					// skip characters until hit a special one or vice versa
+					while ( currentMode == isNormalChar( followingChar ) && followingChar != " ") {
+
+						navPrevChar(editor);
+
+						followingChar = currentLineStr.charAt( editor.getCursor().ch-1 )
+					}
+				}
+				repeatUntilKeyUp(code, this)
+			}
 		});
 
+		this.addCommand({
+			id: 'delete-next-character',
+			name: 'Delete next character',
+			editorCallback: (editor: Editor) => {
+
+				function code(): void {
+
+					deleteNextChar(editor)
+				}
+
+				repeatUntilKeyUp(code, this)
+			}
+		});
+
+		this.addCommand({
+			id: 'delete-previous-char',
+			name: 'Delete previous character',
+			editorCallback: (editor: Editor) => {
+
+				function code(): void {
+
+					deletePrevChar(editor)
+				}
+
+				repeatUntilKeyUp(code, this)
+			}
+		});
+
+		this.addCommand({
+			id: 'delete-next-word',
+			name: 'Delete next word',
+			editorCallback: (editor: Editor) => {
+
+				/* METHOD
+				* 2 modes, special char mode (no 0) and normal char mode (no 1).
+				* skip white spaces only as first chars otherwise stop
+				* */
+				function code(): void {
+
+					const SPECIAL_CHAR_WORD = 0
+					const NORMAL_CHAR_WORD = 1
+
+					let currentLineStr = editor.getLine(editor.getCursor().line)
+
+					// check if at the end of line
+					if ( currentLineStr.length <= editor.getCursor().ch ) {
+
+						deleteNextChar(editor) // moves to next line
+
+						return
+					}
+
+					let followingChar = currentLineStr.charAt( editor.getCursor().ch );
+					let currentMode = NORMAL_CHAR_WORD;
+
+					// first skip initial whitespaces
+					while ( followingChar == " ") {
+
+						deleteNextChar(editor);
+
+						followingChar = currentLineStr.charAt( editor.getCursor().ch );
+					}
+
+					if ( !isNormalChar(followingChar) ) currentMode = SPECIAL_CHAR_WORD;
+
+					// skip characters until hit a special one or vice versa
+					while ( currentMode == isNormalChar( followingChar ) && followingChar != " ") {
+
+						deleteNextChar(editor);
+
+						followingChar = currentLineStr.charAt( editor.getCursor().ch )
+					}
+				}
+				repeatUntilKeyUp(code, this)
+			}
+		})
+		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+		// Using this function will automatically remove the event listener when this plugin is disabled.
+		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+		// 	console.log('click', evt);
+		// });
+
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerInterval(window.setInterval(() => 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -91,47 +238,96 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+// global functions
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+function isNormalChar(char: string): number {
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	if ( "~`!@#$%^&*()-_=+\\|]}[{'\";:/?.>,<".includes(char) ) return 0
+	return 1
+}
+
+function navPrevChar(editor: Editor) {
+
+	let cursorLocationBefore = editor.getCursor()
+
+	if (editor.getCursor().ch == 0) {
+
+		editor.setCursor( cursorLocationBefore.line - 1, editor.getLine(cursorLocationBefore.line - 1).length )
+
+	} else {
+
+		editor.setCursor( cursorLocationBefore.line, cursorLocationBefore.ch - 1 );
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+function navNextChar(editor: Editor) {
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+	let cursorLocationBefore = editor.getCursor()
+	editor.setCursor( cursorLocationBefore.line, cursorLocationBefore.ch + 1 );
+}
+
+function deletePrevChar(editor: Editor) {
+
+	if ( editor.getSelection().length > 0 ) {
+
+		editor.replaceSelection("")
+		return
 	}
 
-	display(): void {
-		const {containerEl} = this;
+	let cursorLocation = editor.getCursor()
+	let cursorPrevious = editor.getCursor()
+	cursorPrevious.ch -= 1
 
-		containerEl.empty();
+	// for some reason it hits an error when deleting at the beginning,
+	// I had to add this if statement to fix that and delete the line that way
+	if ( cursorPrevious.ch <= -1 ) {
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		cursorPrevious.line --;
+		cursorPrevious.ch = editor.getLine(cursorPrevious.line).length;
 	}
+	editor.replaceRange("", cursorPrevious, cursorLocation)
+}
+
+function deleteNextChar(editor: Editor) {
+
+	if ( editor.getSelection().length > 0 ) {
+
+		editor.replaceSelection("")
+		return
+	}
+
+	let cursorLocation = editor.getCursor()
+	let cursorNext = editor.getCursor()
+	cursorNext.ch += 1
+	editor.replaceRange("", cursorLocation, cursorNext )
+}
+
+async function delay(ms: number) {
+
+	return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
+async function repeatUntilKeyUp(code: Function, context: Plugin) {
+
+	let yes = true
+
+	context.registerDomEvent(document, 'keyup', () => {
+
+		yes = false;
+	})
+
+	code()
+	await delay(500)
+	let count = 0
+	while (yes) {
+		await delay(30)
+		code()
+		count++
+		print("repeat")
+	}
+}
+
+function print(str: any) {
+
+	console.log(str)
 }
