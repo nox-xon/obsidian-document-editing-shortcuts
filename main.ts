5f12ef5d5467f35e858cfacf7aae24505648b75f
Your Name <you@example.com>
2022-07-28 22:44:39 +0300

    finished initial functionality

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {json} from "stream/consumers";
 
 // Remember to rename these classes and interfaces!
 
@@ -114,25 +115,25 @@ export default class MyPlugin extends Plugin {
 						return
 					}
 
-					let followingChar = currentLineStr.charAt( editor.getCursor().ch-1 );
+					let prevChar = currentLineStr.charAt( editor.getCursor().ch-1 );
 					let currentMode = NORMAL_CHAR_WORD;
 
 					// first skip initial whitespaces
-					while ( followingChar == " ") {
+					while ( prevChar == " ") {
 
 						navPrevChar(editor);
 
-						followingChar = currentLineStr.charAt( editor.getCursor().ch-1 );
+						prevChar = currentLineStr.charAt( editor.getCursor().ch-1 );
 					}
 
-					if ( !isNormalChar(followingChar) ) currentMode = SPECIAL_CHAR_WORD;
+					if ( !isNormalChar(prevChar) ) currentMode = SPECIAL_CHAR_WORD;
 
 					// skip characters until hit a special one or vice versa
-					while ( currentMode == isNormalChar( followingChar ) && followingChar != " ") {
+					while ( currentMode == isNormalChar( prevChar ) && prevChar != " ") {
 
 						navPrevChar(editor);
 
-						followingChar = currentLineStr.charAt( editor.getCursor().ch-1 )
+						prevChar = currentLineStr.charAt( editor.getCursor().ch-1 )
 					}
 				}
 				repeatUntilKeyUp(code, this)
@@ -178,43 +179,196 @@ export default class MyPlugin extends Plugin {
 				* */
 				function code(): void {
 
+					let cursor = editor.getCursor()
+
+					let line = editor.getLine( cursor.line )
+
 					const SPECIAL_CHAR_WORD = 0
 					const NORMAL_CHAR_WORD = 1
 
-					let currentLineStr = editor.getLine(editor.getCursor().line)
+					let currentMode = NORMAL_CHAR_WORD
 
-					// check if at the end of line
-					if ( currentLineStr.length <= editor.getCursor().ch ) {
+					let followingChar = line.charAt(cursor.ch)
+					let nextCursor = editor.getCursor()
 
-						deleteNextChar(editor) // moves to next line
+					// checking if we are at the last index of the line or one before
+					// if yes, then we only need to delete one char forward (could be the next line)
+					if ( line.length <= cursor.ch || line.length <= cursor.ch +1 ) {
 
+						deleteNextChar(editor)
 						return
 					}
 
-					let followingChar = currentLineStr.charAt( editor.getCursor().ch );
-					let currentMode = NORMAL_CHAR_WORD;
+					if (line.charAt( nextCursor.ch+1 ) == " " ) {
 
-					// first skip initial whitespaces
-					while ( followingChar == " ") {
+						nextCursor.ch ++
 
-						deleteNextChar(editor);
+						while (line.charAt( nextCursor.ch ) == " " ) {
 
-						followingChar = currentLineStr.charAt( editor.getCursor().ch );
+							nextCursor.ch ++
+						}
+						nextCursor.ch--;
+
+						editor.replaceRange( "", cursor, nextCursor)
+
+						print("replaced indexes " + cursor.ch + " to " + nextCursor.ch + " with nothing")
+						return
 					}
 
-					if ( !isNormalChar(followingChar) ) currentMode = SPECIAL_CHAR_WORD;
 
-					// skip characters until hit a special one or vice versa
-					while ( currentMode == isNormalChar( followingChar ) && followingChar != " ") {
+					 if ( followingChar == " " ) {
 
-						deleteNextChar(editor);
+						 nextCursor.ch++;
+						 followingChar = line.charAt(nextCursor.ch)
+					 }
 
-						followingChar = currentLineStr.charAt( editor.getCursor().ch )
+					 if ( !isNormalChar(followingChar) ) currentMode = SPECIAL_CHAR_WORD;
+
+					while ( currentMode == isNormalChar( followingChar )
+					&& followingChar != " "
+					&& line.length > nextCursor.ch) {
+
+						nextCursor.ch++
+						followingChar = line.charAt(nextCursor.ch)
 					}
+					editor.replaceRange( "", cursor, nextCursor)
+
 				}
 				repeatUntilKeyUp(code, this)
 			}
 		})
+
+		this.addCommand({
+			id: 'delete-previous-word',
+			name: 'Delete previous word',
+			editorCallback: (editor: Editor) => {
+
+				/* METHOD
+				* 2 modes, special char mode (no 0) and normal char mode (no 1).
+				* skip white spaces only as first chars otherwise stop
+				* */
+				function code(): void {
+
+					let cursor = editor.getCursor()
+
+					let line = editor.getLine( cursor.line )
+
+					const SPECIAL_CHAR_WORD = 0
+					const NORMAL_CHAR_WORD = 1
+
+					let currentMode = NORMAL_CHAR_WORD
+
+					let prevChar = line.charAt( cursor.ch-1 )
+					let prevCursor = editor.getCursor()
+
+					// checking if we are at the last index of the line or one before
+					// if yes, then we only need to delete one char forward (could be the next line)
+					if ( 0 == cursor.ch || 0 == cursor.ch -1 ) {
+
+						deletePrevChar(editor)
+						return
+					}
+
+					if (line.charAt( prevCursor.ch ) == " " ) {
+
+						prevCursor.ch --
+
+						while (line.charAt( prevCursor.ch ) == " " ) {
+
+							prevCursor.ch --
+						}
+						prevCursor.ch++;
+
+						editor.replaceRange( "", prevCursor, cursor)
+
+						print("replaced indexes " + cursor.ch + " to " + prevCursor.ch + " with nothing")
+						return
+					}
+
+
+					if ( prevChar == " " ) {
+
+						prevCursor.ch--;
+						prevChar = line.charAt(prevCursor.ch-1)
+					}
+
+					if ( !isNormalChar(prevChar) ) currentMode = SPECIAL_CHAR_WORD;
+
+					print("ive reached this point")
+
+					while ( currentMode == isNormalChar( prevChar )
+					&& prevChar != " "
+					&& 0 < prevCursor.ch) {
+
+						prevCursor.ch--
+						prevChar = line.charAt(prevCursor.ch-1)
+					}
+					editor.replaceRange( "", prevCursor, cursor)
+					print("replaced indexes " + cursor.ch + " to " + prevCursor.ch + " with nothing")
+
+				}
+				repeatUntilKeyUp(code, this)
+			}
+		})
+
+		this.addCommand({
+			id: 'navigate-line-up',
+			name: 'Navigate line up',
+			editorCallback: editor => {
+
+				function code(): void {
+
+					editor.exec('goUp')
+				}
+				repeatUntilKeyUp(code, this)
+			}
+		})
+
+		this.addCommand({
+			id: 'navigate-line-down',
+			name: 'Navigate line down',
+			editorCallback: editor => {
+
+				function code(): void {
+
+					editor.exec('goDown')
+				}
+				repeatUntilKeyUp(code, this)
+			}
+		})
+
+		this.addCommand({
+			id: 'new-line',
+			name: 'New Line',
+			editorCallback: editor => {
+
+				function code(): void {
+
+					editor.exec('newlineAndIndent')
+				}
+				repeatUntilKeyUp(code, this)
+			}
+		})
+
+		this.addCommand({
+			id: 'start-new-line',
+			name: 'Start New Line',
+			editorCallback: editor => {
+
+				function code(): void {
+
+					let cursor = editor.getCursor()
+					let line = editor.getLine( cursor.line )
+
+					editor.setCursor(cursor.line, line.length)
+					print("hello")
+					editor.exec('newlineAndIndent')
+				}
+				repeatUntilKeyUp(code, this)
+			}
+		})
+
+
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
@@ -320,7 +474,7 @@ async function repeatUntilKeyUp(code: Function, context: Plugin) {
 	await delay(500)
 	let count = 0
 	while (yes) {
-		await delay(30)
+		await delay(40)
 		code()
 		count++
 		print("repeat")
